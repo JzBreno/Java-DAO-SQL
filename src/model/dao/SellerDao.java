4f22ffa2c46860ae3cbc8175aebe8cc17a0b83bb
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-09-21 11:21:48 -0300

    implementadores e SellerDao

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,9 +5,11 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
+	
 	void insert(Seller sel);
 	void update(Seller sel);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
+	
 }
