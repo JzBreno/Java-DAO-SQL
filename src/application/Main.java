a44daea619abecf885e4f927737796a09fb9c794
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-09-21 15:27:22 -0300

    implementando primeira instancia de SellerDao no nosso main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +12,10 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        //instanciando desta forma, o programa nao conhece a implementação e apenas conhece a interface
+        SellerDao sellerdao = DaoFactory.createSellerDao();
+       
         
-        System.out.println(seller);
-        System.out.println(obj);
     }
     
    
